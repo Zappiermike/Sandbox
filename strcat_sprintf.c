f36c69085544f020633095c0c7ee0879432a6444
Michael Scheie <mscheie@Michaels-MacBook-Air.local>
2023-09-18 13:00:29 -0500

    strcat_sprintf

---
+++ strcat_sprintf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    struct myStruct
+    {
+        char name[60U];
+        int id;
+        int sys;
+    }
+
+    struct myStruct customStruct[2] = {};
+
+    for(int i = 0; i < 2; i++)
+    {
+        char buf = customStruct[i].name;
+        sprintf(buf, "%s%d%s", "hello", "world", (i+1));
+        printf("%s", buf);
+    }
+}

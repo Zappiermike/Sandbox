41441f91c4676a176d252cc71958f4ebfebb76fb
Michael Scheie <zappiermike@gmail.com>
2023-09-20 12:48:45 -0500

    prior to switching over to jpanel

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -1,49 +1,30 @@
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
+import java.awt.FlowLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import javafx.scene.image.Image;
-
-public class BrickBreaker{
-    public static void DisplayFrame() throws IOException{
+public class BrickBreaker {
+    public static void DisplayFrame() {
         JFrame frame = new JFrame("Brick Breaker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // JLabel emptyLabel = new JLabel(new ImageIcon(ImageIO.read(new File("bb_background.jpg"))));
-        JLabel emptyLabel = new JLabel("");
-
-        emptyLabel.setPreferredSize(new Dimension(550, 600));
-
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-        // Add background image
-        frame.setContentPane(emptyLabel);
-
-        // Display the window
-        frame.pack();
+        frame.setSize(550, 600);
         frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+        JLabel background = new JLabel(new ImageIcon("/Users/mscheie/Documents/Coding/sandbox/bb_background.jpg"));
+        frame.add(background);
+        background.setLayout(new FlowLayout());
+
+        brickSlider slider = new brickSlider();
+        frame.add(slider);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                try {
                     DisplayFrame();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
                 }
-            }
-        });
+            });
+        }
     }
-}
-

08a2bda02eead39bc4daeb1723fe4ab181e77dc9
Michael Scheie <zappiermike@gmail.com>
2023-09-21 10:27:16 -0500

    Remove dead code

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -1,15 +1,10 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.URL;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import javafx.scene.text.Font;
 
 public class BrickBreaker extends JFrame implements KeyListener {
 
@@ -39,22 +34,6 @@ public class BrickBreaker extends JFrame implements KeyListener {
         this.add(backgroundLabel);
 
     }
-    // public static void DisplayFrame() {
-    // JFrame frame = new JFrame("Brick Breaker");
-    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    // frame.setSize(550, 600);
-    // frame.setVisible(true);
-    // frame.setLayout(new BorderLayout());
-    // JLabel background = new JLabel(new
-    // ImageIcon("/Users/mscheie/Documents/Coding/sandbox/bb_background.jpg"));
-    // frame.add(background);
-    // // background.setLayout(new FlowLayout());
-
-    // brickSlider slider = new brickSlider();
-    // slider.paintComponent(null);
-    // frame.add(slider);
-    // }
 
     @Override
     public void keyPressed(KeyEvent e) {
